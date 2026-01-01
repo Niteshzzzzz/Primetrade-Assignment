905821d4a413df582c23699aeab21e08c9723130
Niteshzzzzz <niteshkumarnkzzzzz@gmail.com>
2026-01-01 17:26:37 +0530

    updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const register = async (req, res, next) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
     
@@ -40,7 +40,7 @@ export const login = async (req, res, next) => {
       res.cookie('token', token, {
         httpOnly: true,
         secure: env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
       });
       
